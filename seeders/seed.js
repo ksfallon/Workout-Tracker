9e9e3f50f2bab12d238cc9e5ba531fbe6acdf20f
ksfallon <fallon.katelin@gmail.com>
2021-07-30 13:27:58 -0400

    added my server.js and view.js route

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = require('../models');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase', {
+  process.env.MONGODB_URI || 'mongodb://localhost/workout_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
