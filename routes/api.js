31bac01f04f6d742e6f8b7003d88fa5e99fdac7a
ksfallon <fallon.katelin@gmail.com>
2021-08-03 18:31:59 -0400

    updated routes/api.js so the notes for the incorrect put are at the bottom of page

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,38 @@ const Workouts = require('../models/Workouts')
 
 // I need to create a routes to do GET POST PUT DELETE
 
+//First TO ADD NEW WORKOUT we need a POST to CREATE the NEW WORKOUT
+router.post('/api/workouts', (req, res) => {
+    Workouts.create({})
+    .then((response) => {
+        res.json(response)
+    })
+    .catch((err) => {
+        res.json(err)
+    })
+})
+
+//SECOND TO CONTINUE a workout we need to PUT to UPDATE THE CURRENT WORKOUT
+router.put('/api/workouts/:id', (req, res) => {
+    Workouts.aggregate([
+        {
+            $match: { _id: req.params.id}
+        },
+        {
+            $addFields: {exercises: req.body}
+        }
+    ])
+    .then(response => {
+        res.json(response)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+
+module.exports = router
+
 //FIRST NEED TO GET MOST RECENT WORKOUT AND COMBINE ALL EXERCISES
     // need to be for most recent workout BY DATE
     // add HOW MANY Exercises (length of the array)
@@ -30,39 +62,3 @@ const Workouts = require('../models/Workouts')
 //             res.status(400).json(err)
 //         })
 // })
-
-//THIRD TO ADD NEW WORKOUT we need a POST to CREATE the NEW WORKOUT
-router.post('/api/workouts', (req, res) => {
-    Workouts.create({})
-    .then((response) => {
-        res.json(response)
-    })
-    .catch((err) => {
-        res.json(err)
-    })
-})
-
-//SECOND TO CONTINUE a workout we need to PUT to UPDATE THE CURRENT WORKOUT
-router.put('/api/workouts/:id', (req, res) => {
-    Workouts.aggregate([
-        {
-            $match: {
-                _id: req.params.id
-            }
-        },
-        {
-            $addFields: {
-                exercises: req.body
-            }
-        }
-    ])
-    .then(response => {
-        res.json(response)
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
-})
-
-
-module.exports = router
